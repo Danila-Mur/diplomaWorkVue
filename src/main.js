f3f6024790bab525d261d7d2a43f71c5954e7bd6
Danil Murzin <danil.murzin.200026@gmail.com>
2021-07-19 22:28:34 +0300

    fix db

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import store from './store';
 import dateFilter from '@/filters/date.filter';
 import DatePicker from 'vue2-datepicker';
 import 'materialize-css/dist/js/materialize.min';
-// import 'vue-search-select/dist/VueSearchSelect.css'
 
 export const eventEmitter = new Vue();
 
